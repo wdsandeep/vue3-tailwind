0a409311de4cd0c6f319a4310e33237f6eae5d89
wdsandeep <wd.sandeep@gmail.com>
2021-05-19 15:06:03 +0530

    Initial Template for Vue3 and tailwind css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1 class="my-3 bg-gray-100 rounded-xl p-8">{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
